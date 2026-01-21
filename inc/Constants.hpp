84a855184586d4d938c12998f9522046070a66e2
smoore-a <smoore-a@student.42malaga.com>
2026-01-21 16:55:59 +0100

    change cgi_timeout_cycle to pass stress test

--- inc/Constants.hpp
+++ inc/Constants.hpp
@@ -56,6 +56,6 @@ static const size_t CGI_BUFFER_SIZE = 65536;
 
 // CGI timeout in poll cycles (with 5000ms poll timeout, ~1 cycle = 5 seconds max)
 // Set to 2 cycles for ~10 seconds timeout
-static const int CGI_TIMEOUT_CYCLES = 2;
+static const int CGI_TIMEOUT_CYCLES = 500;
 
 #endif
