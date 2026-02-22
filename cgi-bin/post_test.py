333c2241cbfe567999c16fe1f4d6dc40f7222074
Seilan <seilanmoore@gmail.com>
2026-02-22 21:37:27 +0100

    feature: show raw body

--- cgi-bin/post_test.py
+++ cgi-bin/post_test.py
@@ -5,6 +5,7 @@ from urllib.parse import parse_qs
 method = os.environ.get("REQUEST_METHOD", "GET")
 
 params = {}
+post_body = ""
 if method == "GET":
     query_string = os.environ.get("QUERY_STRING", "")
     params = parse_qs(query_string)
@@ -14,7 +15,7 @@ elif method == "POST":
     params = parse_qs(post_body)
 
 name = params.get("name", ["Unknown"])[0].upper()
-email = params.get("email", ["Unknown"])[0].upper()
+message = params.get("message", ["Unknown"])[0].upper()
 
 print("Content-Type: text/html")
 print()
@@ -25,6 +26,8 @@ print("<head><title>Form Result</title></head>")
 print("<body>")
 print("<h1>Data Received</h1>")
 print(f'<p style="color: red; font-size: 24px; font-weight: bold;">Name: {name}</p>')
-print(f'<p style="color: red; font-size: 24px; font-weight: bold;">Email: {email}</p>')
+print(f'<p style="color: red; font-size: 24px; font-weight: bold;">Message: {message}</p>')
+if post_body != "":
+  print(f'<p style="color: red; font-size: 24px; font-weight: bold;">Raw Body: {post_body}</p>')
 print("</body>")
 print("</html>")
